0800eae2616b3dfab420c1ed6b8aaa851cd80625
Kubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2025-06-07 03:24:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,32 @@
-//Modül sürümü 3.1.5
-
-//Modülün sahibi: kubi#5443 Dangerio studio: https://discord.gg/4Xpwwz6pgN 
-//Black Botlist websitesi: https://blackbotlist.cf/ Black Botlist sunucusu: https://discord.gg/cPe9A9vmaE
-//Nasıl kullanılır: https://blackbotlist.cf/docs
-//Hatamı alıyorsun ilk olarak terminale npm i node-fetch@2.6.1 yaz 
-//Halamı hata alıyorsun O zaman Discord serverimize gelin ve ticket açın!
+//How to use: https://blackbotlist.cf/docs
+//If you're getting an error, first type npm i node-fetch@2.6.1 in your terminal
 
 const fetch = require('node-fetch')
 module.exports = {
 
     get: async function (bot, user) {
-        if (!bot) throw new TypeError('BlackBotlist Api\nLütfen botun idsini giriniz\nBlack botlist yardım sunucundan hatayı sorabilirsin!: https://discord.gg/cPe9A9vmaE\n' + __dirname);
-        if (!user) throw new TypeError('BlackBotlist Api\nLütfen kişi idsini giriniz\nBlack botlist yardım sunucundan hatayı sorabilirsin!: https://discord.gg/cPe9A9vmaE\n' + __dirname);
+        if (!bot || !user) 
+            throw new TypeError('BlackBotlist API\nPlease enter the bot\'s ID\nYou can ask about the error on the Black botlist help server: https://discord.gg/cPe9A9vmaE\n' + __dirname);
         const api = await fetch(`https://blackbotlist.cf/apikeyget`).then(response => response.json());
-        if (api.error) throw "BlackBotlist Api\nApi'de bir hata oluştu\nBlack botlist yardım sunucundan hatayı sorabilirsin!: https://discord.gg/cPe9A9vmaE";
+        if (api.error) throw "BlackBotlist API\nAn error occurred in the API\nYou can ask about the error on the Black botlist help server: https://discord.gg/cPe9A9vmaE";
         var sonuc = undefined;
         if(!api["blackbotlist"]) return sonuc; 
         api["blackbotlist"].map(x =>  {
         if (x.id === bot + "_" + user){
-       if(x.süre > Date.now()) sonuc = "true"
+       if(x.time > Date.now()) sonuc = "true"
         }
      })    
     return sonuc;  
     },
 
     webhook: async function (bot) {
-        if (!bot) throw new TypeError('BlackBotlist Api\nLütfen botun idsini giriniz\nBlack botlist yardım sunucundan hatayı sorabilirsin!: https://discord.gg/cPe9A9vmaE\n' + __dirname);
+        if (!bot) throw new TypeError('BlackBotlist API\nPlease enter the bot\'s ID\nYou can ask about the error on the Black botlist help server: https://discord.gg/cPe9A9vmaE\n' + __dirname);
         const api = await fetch(`https://blackbotlist.cf/apikeywebhook`).then(response => response.json());
-        if (api.error) throw "BlackBotlist Api\nApi'de bir hata oluştu\nBlack botlist yardım sunucundan hatayı sorabilirsin!: https://discord.gg/cPe9A9vmaE";
+        if (api.error) throw "BlackBotlist API\nAn error occurred in the API\nYou can ask about the error on the Black botlist help server: https://discord.gg/cPe9A9vmaE";
             var sonuc = undefined;   
             if(!api[bot]) return sonuc; 
-            var süre = Date.now()-api[bot].süre
-            if (süre < 1000) sonuc = api[bot].id 
+            var time = Date.now()-api[bot].time
+            if (time < 1000) sonuc = api[bot].id 
            return sonuc;    
     }
 }
